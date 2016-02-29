4889e4ab192ac09f49e3243c64b036ff66bb0f31
itzuecs <itzuecs@uw.edu>
2016-02-28 19:40:22 -0800

    Correlation analysis (attempt)

--- Cyprinids_Code.R
+++ Cyprinids_Code.R
@@ -43,6 +43,7 @@ Ratios<- Morph[-empty,4:9]
 LinearM<-Morph[-empty,c(10:19)]
 
 ##Tables with final number of individuals to extract the names
+Original_sp<-Perf[-empty,]
 Perf_Sp<-Perf[-empty,]
 Morph_Sp<-Morph[-empty,]
 
@@ -72,15 +73,75 @@ ordiplot(Perfpca)
 #change species names to numbers to visualize in a plot
 levels(Perf_Sp$species)<-c(1:19)
 ggbiplot(Perfpca, choices=1:2, groups = Perf_Sp$species, 
-         labels= Perf_Sp$species, scale=1, ellipse = T)
+         labels= Perf_Sp$species, scale=1, ellipse = T,
+         legend.text=levels(Original_sp$species))
 
 levels(Morph_Sp$species)<-c(1:19)
 
 ggbiplot(Ratiopca, choices=1:2, groups = Morph_Sp$species, 
          labels= Morph_Sp$species, scale=1, ellipse = T)
 
-ggbiplot(Linearpca, choices=1:2, groups = Perf$species, 
-         labels= Perf$species, scale=1, ellipse = T)
+ggbiplot(Linearpca, choices=1:2, groups = Morph_Sp$species, 
+         labels= Morph_Sp$species, scale=1, ellipse = T)
 
+##Correlation analysis
+
+####try one
+corr(Perf_Num, Ratios)
+corr(Perf_Num, LinearM)
+corr(Ratios, LinearM)
+
+##### try two
+cor(Perf_Num, Ratios, use = "everything",
+    method = c("pearson", "kendall", "spearman"))
+
+
+#####try three
+
+#correlation plot package
+install.packages('corrplot')
+library(corrplot)
+
+# Pie plots for correlation test
+Perf_Ratios <- cor(Perf_Num, Ratios)
+corrplot(Perf_Ratios, method = "pie", tl.col = "black", 
+         tl.srt = 45) 
+#other methods "square", "ellipse", "number","shade", color, pie
+
+Perf_LinearM <- cor(Perf_Num, LinearM)
+corrplot(Perf_LinearM, method = "pie", tl.col = "black", 
+         tl.srt = 45)
+
+colCypr<- c('#d8b365', '#5ab4ac')
+Ratios_LinearM <- cor(Ratios, LinearM)
+corrplot(Ratios_LinearM, method = "pie", tl.col = "black", 
+         tl.srt = 45, col = colCypr, rect.col = F, tl.cex = 1)
+           
+
+####linear regressions 
+cor(Perf_Num)
+pairs(Perf_Num)
+
+cor(LinearM)
+pairs(LinearM)
+
+cor(Ratios)
+pairs(Ratios)
+
+lm.out= lm(vmax~amax, data = Perf_Num)
+summary(lm.out)
+plot(vmax~amax, data = Perf_Num)
+abline(lm.out, col='red')
+
+
+## fail attempt to run the correlations per trait in a loop
+for (i in 1:ncol(Perf_Num))
+  {
+  for(j in 1:nrow(Perf_Num))
+  {
+    lm.out= lm(i~j, data = Perf_Num)
+    plot(i~j, data = Perf_Num)
+  }
+}
 
 
